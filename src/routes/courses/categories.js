fd41d81c791f58f5f1f0984d23ba34f5f76318ea
Alok Hegde <hegdealok@yahoo.com>
2022-08-30 20:16:04 +0530

    Added some routes to update the data and fixed some minor bugs

--- src/routes/courses/categories.js
+++ src/routes/courses/categories.js
@@ -1,55 +1,94 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 require("dotenv/config");
 
-const Category = require('../../models/category_model')
+const Category = require("../../models/category_model");
 
-const verify = require('../../helpers/verify_token')
+const verify = require("../../helpers/verify_token");
 
-const { categoryCreationValidation } = require('../../validation/courses/category_validation')
+const {
+  categoryCreationValidation,
+} = require("../../validation/courses/category_validation");
 
 //Creating new category
 
-router.post('/new', verify, async (req, res) => {
-    //Validating the submitted data
-    const { error } = categoryCreationValidation(req.body);
-    if (error) {
-        return res.status(400).json({ message: error.details[0].message });
-    }
+router.post("/new", verify, async (req, res) => {
+  //Validating the submitted data
+  const { error } = categoryCreationValidation(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
 
-    const { name, description } = req.body;
+  const { name, description } = req.body;
 
-    const categoryData = await Category.findOne({"name":name})
+  const categoryData = await Category.findOne({ name: name });
 
-    if(categoryData){
-        return {"status":"error","message":"Category Already Exists"}
-    }
+  if (categoryData) {
+    return { status: "error", message: "Category Already Exists" };
+  }
 
-    let category = new Category({
-        name: name,
-        description: description
-    })
+  let category = new Category({
+    name: name,
+    description: description,
+  });
 
-    try {
-        savedCategory = await category.save();
-        res.status(200).send({ message: "Category Created successfully" });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: error });
-    }
+  try {
+    savedCategory = await category.save();
+    res.status(200).send({ message: "Category Created successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
+  }
 });
 
+// Getting all categories
+router.get("/", verify, async (req, res) => {
+  try {
+    const data = await Category.find();
+    return res.status(200).json({ categories: data });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error });
+  }
+});
 
-router.get('/', verify, async (req, res) => { 
-    try {
-        const data = await Category.find()
-        return res.status(200).json({ categories: data });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: error });
-    }
-})
+//Updating the category
 
+router.put("/:id", verify, async (req, res) => {
+  // Id validation
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return res.status(400).json({ message: "Invalid Student Id" });
+  }
+  const { id } = req.params;
 
-module.exports = router
+  //Validating the submitted data
+  const { error } = categoryCreationValidation(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  const { name, description } = req.body;
+
+  const categoryData = await Category.findOne({ name: name });
+
+  if (categoryData) {
+    return { status: "error", message: "Category Already Exists" };
+  }
+
+  var data = {
+    name: name,
+    description: description,
+  };
+
+  try {
+    await Category.findByIdAndUpdate(id, data);
+    res.status(200).send({ message: "Category updated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
+  }
+});
+
+module.exports = router;
--- src/routes/registrations/college_registration.js
+++ src/routes/registrations/college_registration.js
@@ -1,69 +1,119 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 require("dotenv/config");
 
-const College = require('../../models/college_data_model')
+const College = require("../../models/college_data_model");
 
-const verify = require('../../helpers/verify_token')
+const verify = require("../../helpers/verify_token");
 
-const { collegeRegisterValidation } = require('../../validation/registration/college_registration_validation')
-
+const {
+  collegeRegisterValidation,
+} = require("../../validation/registration/college_registration_validation");
 
 //Registering new college
 
-router.post('/register', verify, async (req, res) => {
-    //Validating the data before creating the college
+router.post("/register", verify, async (req, res) => {
+  //Validating the data before creating the college
 
-    const { error } = collegeRegisterValidation(req.body);
-    if (error) {
-        return res.status(400).json({ message: error.details[0].message });
+  const { error } = collegeRegisterValidation(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  const { name } = req.body;
+
+  const firstLetterOfCode = name[0];
+
+  var secondLetterOfCode = name[5];
+
+  if (secondLetterOfCode === " ") {
+    secondLetterOfCode = name[6];
+  }
+
+  var collegeCode =
+    firstLetterOfCode.toUpperCase() + secondLetterOfCode.toUpperCase();
+
+  let college = new College({
+    name: name,
+    code: collegeCode,
+  });
+
+  try {
+    collegeData = await College.findOne({ name: name });
+
+    if (collegeData) {
+      return res
+        .status(400)
+        .json({ status: "error", message: "College already exists" });
     }
-
-    const { name } = req.body;
-
-    const firstLetterOfCode = name[0];
-
-    var secondLetterOfCode = name[5];
-
-    if (secondLetterOfCode === " ") {
-        secondLetterOfCode = name[6];
-    }
-
-    var collegeCode = firstLetterOfCode.toUpperCase() + secondLetterOfCode.toUpperCase()
-
-    let college = new College({
-        name: name,
-        code: collegeCode
-    })
-
-    try {
-        collegeData = await College.findOne({"name":name})
-
-        if(collegeData){
-            return res.status(400).json({"status":"error","message":"College already exists"})
-        }
-        savedCollege = await college.save();
-        res.status(200).send({ message: "College registered successfully" });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: error });
-    }
-})
-
+    savedCollege = await college.save();
+    res.status(200).send({ message: "College registered successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
+  }
+});
 
 //Getting all registered colleges
 
-router.get('/allColleges', verify, async (req, res) => {
+router.get("/allColleges", verify, async (req, res) => {
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
 
-    try {
-        const data = await College.find()
-        return res.status(200).json({ colleges: data });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: error });
-    }
-})
+  const startIndex = (page - 1) * limit;
+  try {
+    const data = await College.find().limit(limit).skip(startIndex);
+    return res.status(200).json({ colleges: data });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error });
+  }
+});
 
+// Editing the college
+//Registering new college
+
+router.put("/:id", verify, async (req, res) => {
+  //College Id
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return res.status(400).json({ message: "Invalid College Id" });
+  }
+
+  const { id } = req.params;
+  //Validating the data before creating the college
+
+  const { error } = collegeRegisterValidation(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  const { name } = req.body;
+
+  const firstLetterOfCode = name[0];
+
+  var secondLetterOfCode = name[5];
+
+  if (secondLetterOfCode === " ") {
+    secondLetterOfCode = name[6];
+  }
+
+  var collegeCode =
+    firstLetterOfCode.toUpperCase() + secondLetterOfCode.toUpperCase();
+
+  var data = {
+    name: name,
+    code: collegeCode,
+  };
+
+  try {
+    await College.findByIdAndUpdate(id, data);
+    res.status(200).send({ message: "College updated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
+  }
+});
 
 module.exports = router;
